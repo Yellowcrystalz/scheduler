e8619da11702016c6f41bc7fc818675290ffa52b
yellowcrystalz <jaykaycrystal0914@gmail.com>
2024-10-23 21:40:52 -0500

    (feat): implemented most of booking manager

--- classes/booking_manager.py
+++ classes/booking_manager.py
@@ -2,34 +2,53 @@ from datetime import datetime, timedelta
 
 from database.python.db_manager import DBManager
 
-MAX_BOOKING_TIME = 8.0
+MAX_BOOKING_TIME = 8
+
 
 class BookingManager():
-    async def valid_booking(user_start_date: str, user_end_date: str) -> bool:
-        user_start_datetime = await BookingManager.string_to_date(user_start_date)
-        user_end_datetime = await BookingManager.string_to_date(user_end_date)
+    def check_booking(start: str, end: str, reserved_slots: list) -> int:
+        start_dt: datetime = await BookingManager.string_to_datetime(start)
+        end_dt: datetime = await BookingManager.string_to_datetime(end)
 
-        return user_start_datetime < user_end_datetime
+        if not BookingManager.check_timeframe(start_dt, end_dt):
+            return -1
 
-    # async def can_book(user_start_date: str, user_end_date: str) -> bool:
-        # for dates in bookings:
-        #     start_date = datetime.strptime(dates[0], "%Y-%m-%d %H:%M") 
-        #     end_date = datetime.strptime(dates[1], "%Y-%m-%d %H:%M") 
+        if not BookingManager.check_duration(start_dt, end_dt):
+            return -2
 
-        #     if start_date <= user_start_date < end_date:
-        #         return False
-        #     elif start_date < user_end_date <= end_date:
-        #         return False
-        
-        # return True
-    
-    async def string_to_date(string_date: str) -> datetime:
-        return datetime.strptime(string_date, "%Y-%m-%d %H:%M") 
-    
-    async def date_difference(start: datetime, end: datetime) -> float:
-        difference: float = 0.0
+        if not BookingManager.check_conflict(start_dt, end_dt, reserved_slots):
+            return -3
+
+        return 0
+
+    def check_timeframe(start_dt: datetime, end_dt: datetime) -> bool:
+        return start_dt < end_dt
+
+    def check_duration(start_dt: datetime, end_dt: datetime) -> bool:
+        difference: int = BookingManager.date_difference(start_dt, end_dt)
+
+        return MAX_BOOKING_TIME >= difference
+
+    def check_conflict(start_dt: datetime, end_dt: datetime, reserved_slots: list) -> bool:
+        for slots in reserved_slots:
+            res_start: datetime = BookingManager.string_to_datetime(slots[0])
+            res_end: datetime = BookingManager.string_to_datetime(slots[1])
+
+            if res_start <= start_dt < res_end:
+                return False
+            elif res_start < end_dt <= res_end:
+                return False
+
+        return True
+
+    def string_to_datetime(string_date: str) -> datetime:
+        return datetime.strptime(string_date, "%Y-%m-%d %H:%M")
+
+    def date_difference(start: datetime, end: datetime) -> int:
+        difference: int = 0
+
         delta_difference: timedelta = end - start
-        difference += delta_difference.days * 48.0
-        difference += delta_difference.seconds / 1800
+        difference += delta_difference.days * 48
+        difference += int(delta_difference.seconds / 1800)
 
         return difference
